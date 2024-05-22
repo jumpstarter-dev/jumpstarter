d56f178e1776bbc4bcbbb94756684ea65dc44049
Nick Cao <nickcao@nichi.co>
2024-05-22 14:59:33 -0400

    Implement a mock driver
    It provides a stub implementation of the harness.Driver and harness.Device interfaces.
    And utilizes VT100 emulation[1] to parse and emulate a console for createAnsibleInventory to work.
    
    It stores persistent data (name/tags) in a json file hardcoded at `/tmp/jumpstarter-mock.json`
    
    The mock driver is not enabled by default and can be built with `go build -tags mock`
    
    [1] https://pkg.go.dev/golang.org/x/term

--- pkg/drivers/all/drivers.go
+++ pkg/drivers/all/drivers.go
@@ -2,6 +2,7 @@ package all
 
 import (
 	_ "github.com/jumpstarter-dev/jumpstarter/pkg/drivers/dutlink-board"
+	_ "github.com/jumpstarter-dev/jumpstarter/pkg/drivers/mock"
 )
 
 // The purpose of this package is to import all the drivers so that they are
