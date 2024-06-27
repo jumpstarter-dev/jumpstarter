02b1b746fbd27cf4b6acb309e5fabfb2d4adad5e
Nick Cao <nickcao@nichi.co>
2024-06-27 11:00:48 -0400

    Add minimal Identity resource definition

--- controller/cmd/main.go
+++ controller/cmd/main.go
@@ -130,6 +130,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Exporter")
 		os.Exit(1)
 	}
+	if err = (&controller.IdentityReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Identity")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
