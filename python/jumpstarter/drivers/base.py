7cf70525efbf65af1e776bac1cba418943ce55df
Nick Cao <nickcao@nichi.co>
2024-07-02 10:50:38 -0400

    Make DriverBase a dataclass containing uuid and labels

--- python/jumpstarter/drivers/base.py
+++ python/jumpstarter/drivers/base.py
@@ -1,6 +1,14 @@
 # This file contains the base class for all jumpstarter drivers
-from abc import ABC,
+from abc import ABC
+from dataclasses import dataclass
+from uuid import UUID, uuid4
 
+
+@dataclass
 class DriverBase(ABC):
-    def __init__(self):
-        pass
+    uuid: UUID
+    labels: dict[str, str]
+
+    def __init__(self, uuid=None, labels={}):
+        self.uuid = uuid or uuid4()
+        self.labels = labels
