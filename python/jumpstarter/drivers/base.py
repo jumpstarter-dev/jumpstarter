726b967078cf4d6d2c728b4a0a23d30c5890c699
Nick Cao <nickcao@nichi.co>
2024-07-02 13:30:36 -0400

    Add method and decorator for defining and invoking drivercall

--- python/jumpstarter/drivers/base.py
+++ python/jumpstarter/drivers/base.py
@@ -1,9 +1,17 @@
 # This file contains the base class for all jumpstarter drivers
 from abc import ABC
+from typing import List, Any
 from dataclasses import dataclass
 from uuid import UUID, uuid4
 
 
+# decorator to mark a method available for driver calls
+def drivercall(func):
+    func.is_drivercall = True
+    return func
+
+
+# base class for all drivers
 @dataclass
 class DriverBase(ABC):
     uuid: UUID
@@ -12,3 +20,14 @@ class DriverBase(ABC):
     def __init__(self, uuid=None, labels={}):
         self.uuid = uuid or uuid4()
         self.labels = labels
+
+    def call(self, method: str, args: List[Any]) -> Any:
+        try:
+            function = getattr(self, method)
+        except AttributeError:
+            raise NotImplementedError("no such drivercall")
+
+        if not getattr(function, "is_drivercall", False):
+            raise NotImplementedError("no such drivercall")
+
+        return function(*args)
