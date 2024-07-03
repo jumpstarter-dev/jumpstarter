98a1a9ddac48cd253a815397ae80b55f22184200
Nick Cao <nickcao@nichi.co>
2024-07-03 10:17:10 -0400

    Auto generate driver calls for properties

--- python/jumpstarter/drivers/serial/base.py
+++ python/jumpstarter/drivers/serial/base.py
@@ -1,8 +1,25 @@
 from abc import abstractmethod
 from ..base import DriverBase, drivercall
+import inspect
 
 
 class Serial(DriverBase):
+    def __init__(self):
+        super().__init__()
+
+        def build_getter(prop):
+            return drivercall(lambda: prop.fget(self))
+
+        def build_setter(prop):
+            return drivercall(lambda x: prop.fset(self, x))
+
+        properties = inspect.getmembers_static(self, lambda m: isinstance(m, property))
+        for name, prop in properties:
+            if prop.fget:
+                setattr(self, "get_" + name, build_getter(prop))
+            if prop.fset:
+                setattr(self, "set_" + name, build_setter(prop))
+
     @property
     def interface(self):
         return "serial"
@@ -19,11 +36,3 @@ class Serial(DriverBase):
     @property
     @abstractmethod
     def baudrate(self) -> int: ...
-
-    @drivercall
-    def get_baudrate(self) -> int:
-        return self.baudrate
-
-    @drivercall
-    def set_baudrate(self, baudrate: int):
-        self.baudrate = baudrate
