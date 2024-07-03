3cdaab8172e12c69d88e492d23d3c447fe283e25
Nick Cao <nickcao@nichi.co>
2024-07-03 13:51:42 -0400

    Factor out driver call invocations

--- python/tests/test_exporter.py
+++ python/tests/test_exporter.py
@@ -18,28 +18,30 @@ def test_exporter():
     with grpc.insecure_channel("localhost:50051") as channel:
         stub = jumpstarter_pb2_grpc.ExporterServiceStub(channel)
         report = stub.GetReport(empty_pb2.Empty())
+
+        def drivercall(device_uuid, driver_method, *args):
+            return json_format.MessageToDict(
+                stub.DriverCall(
+                    jumpstarter_pb2.DriverCallRequest(
+                        device_uuid=device_uuid,
+                        driver_method=driver_method,
+                        args=[
+                            json_format.ParseDict(arg, struct_pb2.Value())
+                            for arg in args
+                        ],
+                    )
+                ).result
+            )
+
         for device in report.device_report:
             match device.driver_interface:
                 case "power":
-                    assert (
-                        json_format.MessageToDict(
-                            stub.DriverCall(
-                                jumpstarter_pb2.DriverCallRequest(
-                                    device_uuid=device.device_uuid,
-                                    driver_method="on",
-                                )
-                            ).result
-                        )
-                        == True
-                    )
-                    assert json_format.MessageToDict(
-                        stub.DriverCall(
-                            jumpstarter_pb2.DriverCallRequest(
-                                device_uuid=device.device_uuid,
-                                driver_method="read",
-                            )
-                        ).result
-                    ) == {"apparent_power": 10.0, "current": 2.0, "voltage": 5.0}
+                    assert drivercall(device.device_uuid, "on") == True
+                    assert drivercall(device.device_uuid, "read") == {
+                        "apparent_power": 10.0,
+                        "current": 2.0,
+                        "voltage": 5.0,
+                    }
                 case _:
                     raise NotImplementedError
 
