8093154cc819ca4ac86114ff0ea202c135060325
Nick Cao <nickcao@nichi.co>
2024-07-10 09:21:34 -0400

    Parameterize setup_exporter

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -1,48 +1,14 @@
 from jumpstarter.exporter import Exporter
-from jumpstarter.drivers.power import MockPower
-from jumpstarter.drivers.serial import MockSerial
-from jumpstarter.drivers.composite import Composite, Dutlink
 from concurrent import futures
 import pytest
 import grpc
 
 
 @pytest.fixture(scope="module")
-def setup_exporter():
+def setup_exporter(request):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 
-    e = Exporter(
-        devices=[
-            MockPower(
-                labels={
-                    "jumpstarter.dev/name": "power",
-                }
-            ),
-            MockSerial(
-                labels={
-                    "jumpstarter.dev/name": "serial",
-                }
-            ),
-            Composite(
-                labels={
-                    "jumpstarter.dev/name": "composite",
-                },
-                devices=[
-                    MockPower(
-                        labels={
-                            "jumpstarter.dev/name": "power",
-                        }
-                    ),
-                    MockSerial(
-                        labels={
-                            "jumpstarter.dev/name": "serial",
-                        }
-                    ),
-                ],
-            ),
-            Dutlink(labels={"jumpstarter.dev/name": "dutlink"}, serial="c415a913"),
-        ]
-    )
+    e = Exporter(devices=request.param)
     e.add_to_server(server)
 
     server.add_insecure_port("localhost:50051")
--- python/tests/test_exporter.py
+++ python/tests/test_exporter.py
@@ -1,10 +1,49 @@
 from jumpstarter.client import Client
 from jumpstarter.drivers.power import PowerReading
+from jumpstarter.drivers.power import MockPower
+from jumpstarter.drivers.serial import MockSerial
+from jumpstarter.drivers.composite import Composite, Dutlink
 from dataclasses import asdict
 import pytest
 import grpc
 
 
+@pytest.mark.parametrize(
+    "setup_exporter",
+    [
+        [
+            MockPower(
+                labels={
+                    "jumpstarter.dev/name": "power",
+                }
+            ),
+            MockSerial(
+                labels={
+                    "jumpstarter.dev/name": "serial",
+                }
+            ),
+            Composite(
+                labels={
+                    "jumpstarter.dev/name": "composite",
+                },
+                devices=[
+                    MockPower(
+                        labels={
+                            "jumpstarter.dev/name": "power",
+                        }
+                    ),
+                    MockSerial(
+                        labels={
+                            "jumpstarter.dev/name": "serial",
+                        }
+                    ),
+                ],
+            ),
+            Dutlink(labels={"jumpstarter.dev/name": "dutlink"}, serial="c415a913"),
+        ]
+    ],
+    indirect=True,
+)
 def test_exporter(setup_exporter):
     with grpc.insecure_channel("localhost:50051") as channel:
         client = Client(channel)
