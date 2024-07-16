b888ceaec6bf08b8e856b4f051f474a96a12cfea
Miguel Angel Ajo Pelayo <majopela@redhat.com>
2024-07-16 22:57:07 +0200

    Add config handling for client

---
+++ python/jumpstarter/client/config.py
@@ -0,0 +1,39 @@
+import os
+
+import yaml
+
+_CONFIG_PATH = os.path.join(os.path.expanduser('~'), ".config", "jumpstarter")
+
+class Config:
+    def __init__(self, context: str=None):
+        self.endpoint = os.environ.get("JUMPSTARTER_ENDPOINT") or None
+        self.token = os.environ.get("JUMPSTARTER_TOKEN") or None
+        self.name = "client"
+        self.context = context or os.environ.get("JUMPSTARTER_CONTEXT") or None
+        if self.endpoint == None or self.token == None:
+            self._load(self._filename)
+
+    @property
+    def _filename(self):
+        if os.environ.get("JUMPSTARTER_CONFIG"):
+            return os.environ.get("JUMPSTARTER_CONFIG")
+
+        if self.context:
+            return os.path.join(_CONFIG_PATH, "config_{}.yaml".format(self.context))
+        return os.path.join(_CONFIG_PATH, "config.yaml")
+
+    def _load(self, path):
+        with open(path) as f:
+            config = yaml.safe_load(f)
+            client = config.get("client")
+            print(client)
+            if client is None:
+                raise ValueError("config file {} does not contain a 'client' key".format(path))
+            
+            self.endpoint = client.get("endpoint")
+            self.token = client.get("token")
+            self.name = client.get("name") or "client"
+
+            if self.endpoint is None or self.token is None:
+                raise ValueError("config file {} does not contain 'client.endpoint' or 'client.token' keys".format(path))
+
---
+++ python/tests/test_client_config.py
@@ -0,0 +1,118 @@
+import os
+import tempfile
+from unittest.mock import patch
+
+import pytest
+
+from jumpstarter.client.config import Config
+
+
+def setup_function():
+    for key in ["JUMPSTARTER_ENDPOINT", "JUMPSTARTER_TOKEN",
+                "JUMPSTARTER_CONFIG", "JUMPSTARTER_CONTEXT"]:
+        if key in os.environ:
+            del os.environ[key]
+
+
+def test_config_file_path_from_env():
+    os.environ["JUMPSTARTER_CONFIG"] = "/tmp/config.yaml"
+    os.environ["JUMPSTARTER_CONTEXT"] = "dummy_context"
+    with patch.object(Config, '_load', return_value=None) as _:
+        config = Config()
+        assert config._filename == "/tmp/config.yaml"
+
+def test_config_file_path_without_context():
+    os.environ["JUMPSTARTER_CONFIG"] = ""
+    os.environ["JUMPSTARTER_CONTEXT"] = ""
+    with patch.object(Config, '_load', return_value=None) as _:
+        config = Config()
+        assert config._filename.endswith("/.config/jumpstarter/config.yaml") 
+
+def test_config_file_path_with_context():
+    os.environ["JUMPSTARTER_CONFIG"] = ""
+    os.environ["JUMPSTARTER_CONTEXT"] = "mycontext"
+    with patch.object(Config, '_load', return_value=None) as _:
+        config = Config()
+        assert config._filename.endswith("/.config/jumpstarter/config_mycontext.yaml") 
+
+def test_config_file_path_with_argument_context():
+    os.environ["JUMPSTARTER_CONFIG"] = ""
+    os.environ["JUMPSTARTER_CONTEXT"] = "mycontext"
+    with patch.object(Config, '_load', return_value=None) as _:
+        config = Config(context="othercontext")
+        assert config._filename.endswith("/.config/jumpstarter/config_othercontext.yaml") 
+
+
+def test_config_from_env():
+    os.environ["JUMPSTARTER_ENDPOINT"] = "localhost:8080"
+    os.environ["JUMPSTARTER_TOKEN"] = "mytoken"
+    config = Config()
+    assert config.endpoint == "localhost:8080"
+    assert config.token == "mytoken"
+    assert config.name == "client"
+
+def test_config_from_file():
+    CONFIG = """
+client:
+    endpoint: yaml-host:8080
+    token: yaml-token
+    name: yaml-client
+"""
+
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as f:
+        f.write(CONFIG)
+        f.close()
+        os.environ["JUMPSTARTER_CONFIG"] = f.name
+        config = Config()
+        assert config.endpoint == "yaml-host:8080"
+        assert config.token == "yaml-token"
+        assert config.name == "yaml-client"
+        os.unlink(f.name)
+
+def test_config_from_file_with_missing_token():
+    CONFIG = """
+client:
+    endpoint: yaml-host:8080
+    name: yaml-client
+"""
+
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as f:
+        f.write(CONFIG)
+        f.close()
+        os.environ["JUMPSTARTER_CONFIG"] = f.name
+        with pytest.raises(ValueError):
+            Config()
+        os.unlink(f.name)
+    
+def test_config_from_file_with_missing_endpoint():
+    CONFIG = """
+client:
+    token: abcd
+    name: yaml-client
+"""
+
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as f:
+        f.write(CONFIG)
+        f.close()
+        os.environ["JUMPSTARTER_CONFIG"] = f.name
+        with pytest.raises(ValueError):
+            Config()
+        os.unlink(f.name)
+
+
+def test_config_from_file_with_missing_name():
+    CONFIG = """
+client:
+    endpoint: yaml-host:8080
+    token: yaml-token
+"""
+
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as f:
+        f.write(CONFIG)
+        f.close()
+        os.environ["JUMPSTARTER_CONFIG"] = f.name
+        config = Config()
+        assert config.endpoint == "yaml-host:8080"
+        assert config.token == "yaml-token"
+        assert config.name == "client"
+        os.unlink(f.name)
