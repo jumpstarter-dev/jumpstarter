7e4c884d8e0b557a7917078bdc5277ac6ac0ff45
Nick Cao <nickcao@nichi.co>
2024-07-17 15:11:37 -0400

    Revert "Drop unnecessary try block in forward_server_stream"
    This reverts commit 94dc04a3a9c961be922d52b3b7ee7cbe980a5185.

--- python/jumpstarter/common/streams.py
+++ python/jumpstarter/common/streams.py
@@ -5,17 +5,25 @@ import grpc
 
 
 async def forward_server_stream(request_iterator, stream):
-    async def client_to_server():
-        async for frame in request_iterator:
-            await stream.send(frame.payload)
-        await stream.send_eof()
-
     async with create_task_group() as tg:
+
+        async def client_to_server():
+            try:
+                async for frame in request_iterator:
+                    await stream.send(frame.payload)
+            except BrokenResourceError:
+                pass
+            finally:
+                await stream.send_eof()
+
         tg.start_soon(client_to_server)
 
         # server_to_client
-        async for payload in stream:
-            yield router_pb2.StreamResponse(payload=payload)
+        try:
+            async for payload in stream:
+                yield router_pb2.StreamResponse(payload=payload)
+        except BrokenResourceError:
+            pass
 
 
 async def forward_client_stream(router, stream, metadata):
