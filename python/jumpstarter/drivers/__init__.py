6dad679d1dde5ae1446917f49c0ffdaee7b69d4f
Nick Cao <nickcao@nichi.co>
2024-07-22 14:47:15 -0400

    Drop the old driver registry

--- python/jumpstarter/drivers/__init__.py
+++ python/jumpstarter/drivers/__init__.py
@@ -4,32 +4,6 @@ from .base import (
     DriverClient,
     Store,
 )
-from .registry import _registry
-
-
-def register(cls):
-    _registry.register(cls)
-
-
-def get(name):
-    return _registry.get(name)
-
-
-def __iter__():
-    return iter(_registry)
-
-
-def __len__():
-    return len(_registry)
-
-
-def __getitem__(name):
-    return _registry.get(name)
-
-
-def __contains__(name):
-    return name in _registry
-
 
 __all__ = [
     "Store",
