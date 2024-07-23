20b5db72f0519ce040ac46dbf14c0f81a02f60d4
Nick Cao <nickcao@nichi.co>
2024-07-23 15:11:03 -0400

    Implement a dummy shell subcommand

--- python/jumpstarter/cli/shell.py
+++ python/jumpstarter/cli/shell.py
@@ -1,6 +1,55 @@
+import os
+import sys
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+import anyio
 import click
+import grpc
+
+from jumpstarter.drivers.composite import Composite
+from jumpstarter.drivers.network import EchoNetwork
+from jumpstarter.drivers.power import MockPower
+from jumpstarter.exporter import Session
+
+
+async def shell_impl():
+    server = grpc.aio.server()
+
+    session = Session(
+        labels={"jumpstarter.dev/name": "transient"},
+        root_device=Composite(
+            labels={"jumpstarter.dev/name": "transient"},
+            children=[
+                MockPower(labels={"jumpstarter.dev/name": "power"}),
+                EchoNetwork(labels={"jumpstarter.dev/name": "echo"}),
+            ],
+        ),
+    )
+    session.add_to_server(server)
+
+    with TemporaryDirectory() as tempdir:
+        socketpath = Path(tempdir) / "socket"
+        server.add_insecure_port(f"unix://{socketpath}")
+
+        await server.start()
+
+        async with await anyio.open_process(
+            [os.environ["SHELL"]],
+            stdin=sys.stdin,
+            stdout=sys.stdout,
+            stderr=sys.stderr,
+            env=os.environ
+            | {
+                "JUMPSTARTER_HOST": f"unix://{socketpath}",
+            },
+        ) as process:
+            await process.wait()
+
+        await server.stop(grace=None)
 
 
 @click.command()
 def shell():
-    pass
+    """Spawns a shell with a transient exporter session"""
+    anyio.run(shell_impl)
