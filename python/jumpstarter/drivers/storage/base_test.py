016949eb9c41f09bd7e3765d3ad45fd9ca0e3eef
Nick Cao <nickcao@nichi.co>
2024-07-25 11:12:43 -0400

    Fix test on python < 3.12

--- python/jumpstarter/drivers/storage/base_test.py
+++ python/jumpstarter/drivers/storage/base_test.py
@@ -1,4 +1,5 @@
 from tempfile import NamedTemporaryFile
+import os
 
 import pytest
 
@@ -10,9 +11,11 @@ pytestmark = pytest.mark.anyio
 
 async def test_drivers_mock_storage_mux():
     async with serve(MockStorageMux(name="storage")) as client:
-        with NamedTemporaryFile(delete_on_close=False) as file:
+        with NamedTemporaryFile(delete=False) as file:
             file.write(b"testcontent" * 1000)
             file.close()
 
             async with client.local_file(file.name) as handle:
                 await client.write(handle)
+
+            os.unlink(file.name)
