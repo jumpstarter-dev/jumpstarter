4fe21be543cbc9f5e659547f6090514051c247d2
Nick Cao <nickcao@nichi.co>
2024-07-30 14:42:16 -0400

    Test console with pexpect

--- python/jumpstarter/drivers/dutlink/base_test.py
+++ python/jumpstarter/drivers/dutlink/base_test.py
@@ -1,4 +1,10 @@
+import socket
+from pathlib import Path
+from tempfile import TemporaryDirectory
 from time import sleep
+
+import anyio
+from pexpect.fdpexpect import fdspawn
 
 from jumpstarter.common.utils import serve
 from jumpstarter.drivers.dutlink.base import Dutlink
@@ -23,5 +29,15 @@ def test_drivers_dutlink():
 
         client.storage.write("/dev/null")
 
-        with client.console.connect() as stream:
-            pass
+        with TemporaryDirectory() as tempdir:
+            socketpath = Path(tempdir) / "socket"
+
+            listener = client.portal.call(anyio.create_unix_listener, socketpath)
+
+            with client.console.portforward(listener):
+                with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
+                    s.connect(str(socketpath))
+
+                    expect = fdspawn(s)
+                    expect.send("about\r\n")
+                    expect.expect("Jumpstarter test-harness")
