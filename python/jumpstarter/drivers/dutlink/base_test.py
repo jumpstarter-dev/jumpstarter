7386a2ede0b4c233a6868a3a9ce4da67d6c5c16b
Nick Cao <nickcao@nichi.co>
2024-07-30 16:22:04 -0400

    Drop ununsed imports

--- python/jumpstarter/drivers/dutlink/base_test.py
+++ python/jumpstarter/drivers/dutlink/base_test.py
@@ -1,10 +1,3 @@
-import socket
-from pathlib import Path
-from tempfile import TemporaryDirectory
-
-import anyio
-from pexpect.fdpexpect import fdspawn
-
 from jumpstarter.common.utils import serve
 from jumpstarter.drivers.dutlink.base import Dutlink
 
@@ -16,28 +9,23 @@ def test_drivers_dutlink():
             storage_device="/dev/null",
         )
     ) as client:
-        with TemporaryDirectory() as tempdir:
-            socketpath = Path(tempdir) / "socket"
+        with client.console.expect() as expect:
+            expect.send("\x02" * 5)
 
-            listener = client.portal.call(anyio.create_unix_listener, socketpath)
+            expect.send("about\r\n")
+            expect.expect("Jumpstarter test-harness")
 
-            with client.console.expect() as expect:
-                expect.send("\x02" * 5)
+            expect.send("console\r\n")
+            expect.expect("Entering console mode")
 
-                expect.send("about\r\n")
-                expect.expect("Jumpstarter test-harness")
+            client.power.off()
 
-                expect.send("console\r\n")
-                expect.expect("Entering console mode")
+            client.storage.write("/dev/null")
+            client.storage.dut()
 
-                client.power.off()
+            client.power.on()
 
-                client.storage.write("/dev/null")
-                client.storage.dut()
+            expect.send("\x02" * 5)
+            expect.expect("Exiting console mode")
 
-                client.power.on()
-
-                expect.send("\x02" * 5)
-                expect.expect("Exiting console mode")
-
-                client.power.off()
+            client.power.off()
