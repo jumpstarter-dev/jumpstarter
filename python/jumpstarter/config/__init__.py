665e555cfa5a47e0fd9dbc5e6f070b3c47ea05a2
Nick Cao <nickcao@nichi.co>
2024-08-12 13:51:59 -0400

    Reimplement UserConfig with pydantic

--- python/jumpstarter/config/__init__.py
+++ python/jumpstarter/config/__init__.py
@@ -5,7 +5,7 @@ from .client import (
 )
 from .common import CONFIG_API_VERSION, CONFIG_PATH
 from .env import JMP_CLIENT_CONFIG, JMP_DRIVERS_ALLOW, JMP_ENDPOINT, JMP_TOKEN
-from .user import UserConfig
+from .user import UserConfigV1Alpha1, UserConfigV1Alpha1Config
 
 __all__ = [
     "CONFIG_API_VERSION",
@@ -15,7 +15,8 @@ __all__ = [
     "JMP_TOKEN",
     "JMP_DRIVERS_ALLOW",
     "JMP_DRIVERS_ALLOW_UNSAFE",
-    "UserConfig",
+    "UserConfigV1Alpha1",
+    "UserConfigV1Alpha1Config",
     "ClientConfigV1Alpha1",
     "ClientConfigV1Alpha1Client",
     "ClientConfigV1Alpha1Drivers",
