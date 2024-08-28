284328360000c063c545f6c468331cf33ef8efad
Nick Cao <nickcao@nichi.co>
2024-08-28 11:06:08 -0400

    Implement NovncAdapter

--- python/jumpstarter/client/adapters/__init__.py
+++ python/jumpstarter/client/adapters/__init__.py
@@ -1,4 +1,5 @@
 from .common import ClientAdapter
+from .novnc import NovncAdapter
 from .portforward import PortforwardAdapter
 
-__all__ = ["ClientAdapter", "PortforwardAdapter"]
+__all__ = ["ClientAdapter", "PortforwardAdapter", "NovncAdapter"]
---
+++ python/jumpstarter/client/adapters/novnc.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from urllib.parse import urlencode, urlunparse
+
+from anyio import sleep_forever
+
+from jumpstarter.streams import WebsocketServerStream, forward_stream
+
+from .portforward import PortforwardAdapter
+
+
+@dataclass(kw_only=True)
+class NovncAdapter(PortforwardAdapter):
+    async def __aenter__(self):
+        addr = await super().__aenter__()
+        return urlunparse(
+            (
+                "https",
+                "novnc.com",
+                "/noVNC/vnc.html",
+                "",
+                urlencode({"autoconnect": 1, "reconnect": 1, "host": addr[0], "port": addr[1]}),
+                "",
+            )
+        )
+
+    async def handler(self, conn):
+        async with conn:
+            async with self.client.stream_async(self.method) as stream:
+                async with WebsocketServerStream(stream=stream) as stream:
+                    async with forward_stream(conn, stream):
+                        await sleep_forever()
