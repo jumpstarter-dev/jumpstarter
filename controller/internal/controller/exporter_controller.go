8e422ae77546ff84e5f9c7fccc88e3223266a747
Nick Cao <nickcao@nichi.co>
2024-09-17 08:41:49 -0400

    Drop unused rbac rules and service accounts

--- controller/internal/controller/exporter_controller.go
+++ controller/internal/controller/exporter_controller.go
@@ -43,9 +43,6 @@ type ExporterReconciler struct {
 // +kubebuilder:rbac:groups=jumpstarter.dev,resources=exporters/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=jumpstarter.dev,resources=exporters/finalizers,verbs=update
 // +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;delete
-// +kubebuilder:rbac:groups=core,resources=serviceaccounts,verbs=get;list;watch
-// +kubebuilder:rbac:groups=core,resources=serviceaccounts/token,verbs=create
-// +kubebuilder:rbac:groups=authentication.k8s.io,resources=tokenreviews,verbs=create
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
