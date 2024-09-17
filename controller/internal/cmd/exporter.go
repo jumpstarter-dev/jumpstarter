a6ac5de3eb7e2c407fb49fff2117094432e8d781
Nick Cao <nickcao@nichi.co>
2024-09-17 10:38:36 -0400

    Move common code for cmd to root.go

--- controller/internal/cmd/exporter.go
+++ controller/internal/cmd/exporter.go
@@ -1,11 +1,8 @@
 package cmd
 
 import (
-	"context"
 	"fmt"
 	"os"
-	"path/filepath"
-	"time"
 
 	jumpstarterdevv1alpha1 "github.com/jumpstarter-dev/jumpstarter-controller/api/v1alpha1"
 	"github.com/spf13/cobra"
@@ -14,63 +11,21 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/types"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/cli-runtime/pkg/printers"
-	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-var (
-	kubeconfig string
-	namespace  string
-	timeout    string
-)
-
 func init() {
-	utilruntime.Must(jumpstarterdevv1alpha1.AddToScheme(scheme.Scheme))
-
 	rootCmd.AddCommand(exporterCmd)
 
-	exporterCmd.PersistentFlags().StringVar(&kubeconfig, "kubeconfig", filepath.Join(homedir.HomeDir(), ".kube", "config"), "Path to the kubeconfig file to use")
-	exporterCmd.PersistentFlags().StringVar(&namespace, "namespace", "default", "Kubernetes namespace to operate on")
-	exporterCmd.PersistentFlags().StringVar(&timeout, "timeout", "10s", "command timeout")
 	exporterCmd.AddCommand(exporterCreateCmd)
 	exporterCmd.AddCommand(exporterDeleteCmd)
 	exporterCmd.AddCommand(exporterListCmd)
 }
 
-type contextKey string
-
 var exporterCmd = &cobra.Command{
 	Use:   "exporter",
 	Short: "Manage exporters",
-	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-		duration, err := time.ParseDuration(timeout)
-		if err != nil {
-			return err
-		}
-
-		ctx, cancel := context.WithTimeout(context.Background(), duration)
-		ctx = context.WithValue(ctx, contextKey("cancel"), cancel)
-		cmd.SetContext(ctx)
-
-		return nil
-	},
-	PersistentPostRunE: func(cmd *cobra.Command, args []string) error {
-		cmd.Context().Value(contextKey("cancel")).(context.CancelFunc)()
-
-		return nil
-	},
-}
-
-func NewClient() (client.WithWatch, error) {
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	if err != nil {
-		return nil, err
-	}
-	return client.NewWithWatch(config, client.Options{Scheme: scheme.Scheme})
 }
 
 var exporterCreateCmd = &cobra.Command{
