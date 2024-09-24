ab6ee42abab83a264a4ec886a0729eb1087eb354
Nick Cao <nickcao@nichi.co>
2024-09-24 12:48:55 -0400

    Add additional debug logging to *Network drivers

--- python/jumpstarter/drivers/network/driver.py
+++ python/jumpstarter/drivers/network/driver.py
@@ -1,3 +1,4 @@
+import logging
 from abc import ABCMeta, abstractmethod
 from contextlib import asynccontextmanager
 from dataclasses import dataclass
@@ -11,6 +12,8 @@ from anyio import (
 from anyio.streams.stapled import StapledObjectStream
 
 from jumpstarter.driver import Driver, exportstream
+
+logger = logging.getLogger(__name__)
 
 
 class NetworkInterface(metaclass=ABCMeta):
@@ -31,6 +34,7 @@ class TcpNetwork(NetworkInterface, Driver):
     @exportstream
     @asynccontextmanager
     async def connect(self):
+        logger.debug("Connecting TCP host=%s port=%d", self.host, self.port)
         async with await connect_tcp(remote_host=self.host, remote_port=self.port) as stream:
             yield stream
 
@@ -43,6 +47,7 @@ class UdpNetwork(NetworkInterface, Driver):
     @exportstream
     @asynccontextmanager
     async def connect(self):
+        logger.debug("Connecting UDP host=%s port=%d", self.host, self.port)
         async with await create_connected_udp_socket(remote_host=self.host, remote_port=self.port) as stream:
             yield stream
 
@@ -54,6 +59,7 @@ class UnixNetwork(NetworkInterface, Driver):
     @exportstream
     @asynccontextmanager
     async def connect(self):
+        logger.debug("Connecting UDS path=%s", self.path)
         async with await connect_unix(path=self.path) as stream:
             yield stream
 
@@ -63,5 +69,6 @@ class EchoNetwork(NetworkInterface, Driver):
     @asynccontextmanager
     async def connect(self):
         tx, rx = create_memory_object_stream[bytes](32)
+        logger.debug("Connecting Echo")
         async with StapledObjectStream(tx, rx) as stream:
             yield stream
