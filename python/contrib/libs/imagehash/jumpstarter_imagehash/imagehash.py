1d47a1e4117b40a3679755d775b43c2dde0f9918
Nick Cao <nickcao@nichi.co>
2024-10-16 09:16:08 -0400

    Make ImageHash a dataclass

--- python/contrib/libs/imagehash/jumpstarter_imagehash/imagehash.py
+++ python/contrib/libs/imagehash/jumpstarter_imagehash/imagehash.py
@@ -1,27 +1,33 @@
 import logging
 import os
+from dataclasses import dataclass
+from typing import Any
 
 import imagehash
 from PIL import Image
 
+from jumpstarter.client import DriverClient
+
 log = logging.getLogger("imagehash")
 
+
+@dataclass
 class ImageHash:
-    """ ImageHash class
+    """ImageHash class
 
     ImageHash class for image hashing and comparison
 
-    param video_client: video client object, must provide a snapshot method
-    type video_client: object
+    param client: video client object, must provide a snapshot method
+    type client: object
     param hash_func: hash function to use from imagehash library, default is average_hash
     type hash_func: function
     param hash_size: hash size to use from imagehash library, default is 8
     type hash_size: int
     """
-    def __init__(self, video_client, hash_func=imagehash.average_hash, hash_size=8):
-        self._client = video_client
-        self._hash_func = hash_func
-        self._hash_size = hash_size
+
+    client: DriverClient
+    hash_func: Any = imagehash.average_hash
+    hash_size: int = 8
 
     def snapshot(self):
         """
@@ -30,7 +36,7 @@ class ImageHash:
         :return: a snapshot image
         :rtype: PIL.Image
         """
-        return self._client.snapshot()
+        return self.client.snapshot()
 
     def hash_snapshot(self):
         """
@@ -39,7 +45,7 @@ class ImageHash:
         :return: hash of the snapshot image
         :rtype: ImageHash
         """
-        return self._hash_func(self.snapshot())
+        return self.hash_func(self.snapshot())
 
     def assert_snapshot(self, reference_img_file, tolerance=1):
         """
@@ -50,7 +56,7 @@ class ImageHash:
 
         :raises AssertionError: if the snapshot image is different from the reference image
         """
-        diff, snapshot_img  = self._snapshot_diff(reference_img_file)
+        diff, snapshot_img = self._snapshot_diff(reference_img_file)
         if diff > tolerance:
             directory = os.getcwd()
             reference_img_file = "FAILED_" + os.path.basename(reference_img_file)
@@ -58,14 +64,17 @@ class ImageHash:
             save_filename = os.path.join(directory, reference_img_file)
             log.error(f"Image hashes are different, saving the actual image as {save_filename}")
             snapshot_img.save(save_filename)
-            raise AssertionError(f"{self._client.name}.assert_snapshot {reference_img_file}:"
-                                 f" diff {diff} > tolerance {tolerance}")
+            raise AssertionError(
+                f"{self.client.name}.assert_snapshot {reference_img_file}:" f" diff {diff} > tolerance {tolerance}"
+            )
 
     def _snapshot_diff(self, reference_img_file, hash_func=imagehash.average_hash, hash_size=8):
         snapshot_img = self.snapshot()
-        ref_hash = self._hash_func(Image.open(reference_img_file), hash_size=self._hash_size)
-        snapshot_hash = self._hash_func(snapshot_img, hash_size=self._hash_size)
+        ref_hash = self.hash_func(Image.open(reference_img_file), hash_size=self.hash_size)
+        snapshot_hash = self.hash_func(snapshot_img, hash_size=self.hash_size)
         diff = ref_hash - snapshot_hash
-        log.info(f"{self._client.name} comparing snapshot {reference_img_file}:"
-                 f" snapshot {snapshot_hash}, ref {ref_hash}, diff: {diff}")
+        log.info(
+            f"{self.client.name} comparing snapshot {reference_img_file}:"
+            f" snapshot {snapshot_hash}, ref {ref_hash}, diff: {diff}"
+        )
         return diff, snapshot_img
--- python/contrib/libs/imagehash/jumpstarter_imagehash/imagehash_test.py
+++ python/contrib/libs/imagehash/jumpstarter_imagehash/imagehash_test.py
@@ -11,9 +11,11 @@ def _image_path(filename):
     current_file_directory = os.path.dirname(__file__)
     return os.path.join(current_file_directory, filename)
 
+
 def test_imagehash_assert_snapshot():
     snapshot_a = SnapshotMock("test_image_a.jpeg")
     ImageHash(snapshot_a).assert_snapshot(_image_path("test_image_a.jpeg"))
+
 
 def test_imagehash_fail_assert_snapshot():
     snapshot_a = SnapshotMock("test_image_a.jpeg")
@@ -21,13 +23,16 @@ def test_imagehash_fail_assert_snapshot():
     with pytest.raises(AssertionError):
         ImageHash(snapshot_a).assert_snapshot(_image_path("test_image_b.jpeg"))
 
+
 def test_imagehash_passthrough_snapshot():
     snapshot_a = SnapshotMock("test_image_a.jpeg")
     assert ImageHash(snapshot_a).snapshot() == snapshot_a.img
 
+
 def test_imagehash_hash_snapshot():
     snapshot_a = SnapshotMock("test_image_a.jpeg")
     assert isinstance(ImageHash(snapshot_a).hash_snapshot(), imagehash.ImageHash)
+
 
 class SnapshotMock:
     def __init__(self, filename):
