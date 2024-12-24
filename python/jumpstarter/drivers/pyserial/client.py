0cd19468261a3c9b21d83224ee7e39339537da42
Miguel Angel Ajo Pelayo <miguelangel@ajo.es>
2024-12-24 17:29:39 +0100

    Add support for console under the j client for PySerialClient
    Enables a cli action for pyserial device in the form of `start-console`.
    
    ```
    $ jmp shell -l board abc
    $$ j serial start-console
    ```

--- python/jumpstarter/drivers/pyserial/client.py
+++ python/jumpstarter/drivers/pyserial/client.py
@@ -1,5 +1,24 @@
+import click
+
 from jumpstarter.client import DriverClient
+
+from .console import Console
 
 
 class PySerialClient(DriverClient):
     pass
+
+    def cli(self):
+        @click.group
+        def base():
+            """Serial port client"""
+            pass
+
+        @base.command()
+        def start_console():
+            """Start serial port console"""
+            click.echo("\nStarting serial port console ... exit with CTRL+B x 3 times\n")
+            console = Console(serial_client=self)
+            console.run()
+
+        return base
