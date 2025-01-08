bcbda6e7ad152e50cdc2af92aaa41eab506e552a
Miguel Angel Ajo Pelayo <majopela@redhat.com>
2025-01-08 10:59:49 +0100

    Fix JMP_LEASE handling in pytest
    1) Look for JMP_LEASE inside the client.lease methods
    2) Only release runtime created leases, leases received via JMP_LEASE
       are kept, because those must be manually released.

--- python/jumpstarter/testing/pytest.py
+++ python/jumpstarter/testing/pytest.py
@@ -1,5 +1,4 @@
 import logging
-import os
 import time
 from typing import ClassVar
 
@@ -59,10 +58,7 @@ i.e.:
         except RuntimeError:
             labels = getattr(self, "filter_labels", {})
             config = ClientConfigV1Alpha1.load("default")
-            lease_name = os.getenv(env.JMP_LEASE, None)
-            if lease_name is not None:
-                log.info(f"Using lease from env var JMP_LEASE: {lease_name}")
-            with config.lease(metadata_filter=MetadataFilter(labels=labels), lease_name=lease_name) as lease:
+            with config.lease(metadata_filter=MetadataFilter(labels=labels)) as lease:
                 with lease.connect() as client:
                     yield client
         # BUG workaround: make sure that grpc servers get the client/lease release properly
