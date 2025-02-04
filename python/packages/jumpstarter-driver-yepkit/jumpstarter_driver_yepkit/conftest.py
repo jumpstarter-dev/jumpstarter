41cdf3ee29a3ce55c3ffb016b99fd35aefbfe2a5
Miguel Angel Ajo Pelayo <majopela@redhat.com>
2025-02-04 13:38:55 +0100

    Add Yepkit YKUSH Usb driver
    This USB Hub enables power control of the ports, making it useful
    to control the power of different targets as long as those targets
    can be USB powered.

---
+++ python/packages/jumpstarter-driver-yepkit/jumpstarter_driver_yepkit/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+import usb
+
+
+def pytest_runtest_call(item):
+    try:
+        item.runtest()
+    except FileNotFoundError:
+        pytest.skip("yepkit not available")
+    except usb.core.USBError:
+        pytest.skip("USB not available, could need root permissions")
+    except usb.core.NoBackendError:
+        pytest.skip("No USB backend")
