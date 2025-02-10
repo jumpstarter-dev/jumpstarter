d33745935b7051f3e793beea97d2e0e507126efe
Nick Cao <nickcao@nichi.co>
2025-02-10 15:07:28 -0500

    Fixup tests

--- python/conftest.py
+++ python/conftest.py
@@ -2,17 +2,20 @@ from contextlib import contextmanager
 
 import pytest
 
-from jumpstarter.common.utils import serve
-from jumpstarter.config import ExporterConfigV1Alpha1DriverInstance
+try:
+    from jumpstarter.common.utils import serve
+    from jumpstarter.config import ExporterConfigV1Alpha1DriverInstance
+except ImportError:
+    # some packages in the workspace does not depend on jumpstarter
+    pass
+else:
 
+    @contextmanager
+    def run(config):
+        with serve(ExporterConfigV1Alpha1DriverInstance.from_str(config).instantiate()) as client:
+            yield client
 
-@contextmanager
-def run(config):
-    with serve(ExporterConfigV1Alpha1DriverInstance.from_str(config).instantiate()) as client:
-        yield client
-
-
-@pytest.fixture(autouse=True)
-def jumpstarter_namespace(doctest_namespace):
-    doctest_namespace["serve"] = serve
-    doctest_namespace["run"] = run
+    @pytest.fixture(autouse=True)
+    def jumpstarter_namespace(doctest_namespace):
+        doctest_namespace["serve"] = serve
+        doctest_namespace["run"] = run
