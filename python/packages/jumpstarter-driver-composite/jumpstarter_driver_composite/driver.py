32116e0e9422327896bf4d2724099c9c4fdbd87f
Nick Cao <nickcao@nichi.co>
2025-02-24 13:16:24 -0500

    Init Proxy driver

--- python/packages/jumpstarter-driver-composite/jumpstarter_driver_composite/driver.py
+++ python/packages/jumpstarter-driver-composite/jumpstarter_driver_composite/driver.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 from pydantic.dataclasses import dataclass
 
 from jumpstarter.driver import Driver
@@ -12,3 +14,21 @@ class CompositeInterface:
 @dataclass(kw_only=True)
 class Composite(CompositeInterface, Driver):
     pass
+
+
+@dataclass(kw_only=True)
+class Proxy(Driver):
+    path: list[str]
+
+    @classmethod
+    def client(cls) -> str:
+        return "jumpstarter.client.DriverClient"  # unused
+
+    def __target(self, root):
+        return reduce(lambda instance, name: instance.children[name], self.path, root)
+
+    def report(self, *, root=None, parent=None, name=None):
+        return self.__target(root).report(root=root, parent=parent, name=name)
+
+    def enumerate(self, *, root=None, parent=None, name=None):
+        return self.__target(root).enumerate(root=root, parent=parent, name=name)
