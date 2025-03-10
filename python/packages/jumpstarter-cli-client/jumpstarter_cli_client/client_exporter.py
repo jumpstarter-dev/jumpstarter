c8cca3d50bbf90977a8d1c61525b66d250a14843
Nick Cao <nickcao@nichi.co>
2025-03-10 14:17:38 -0400

    Implement jmp client list-exporters command

---
+++ python/packages/jumpstarter-cli-client/jumpstarter_cli_client/client_exporter.py
@@ -0,0 +1,45 @@
+import asyncclick as click
+from jumpstarter_cli_common import OutputMode, OutputType, make_table, opt_output_all
+from jumpstarter_cli_common.exceptions import handle_exceptions
+
+from jumpstarter.config import (
+    ClientConfigV1Alpha1,
+    UserConfigV1Alpha1,
+)
+
+
+@click.command("list-exporters", short_help="List available exporters.")
+@click.argument("name", type=str, default="")
+@opt_output_all
+@handle_exceptions
+def list_client_exporters(name: str | None, output: OutputType):
+    if name:
+        config = ClientConfigV1Alpha1.load(name)
+    else:
+        config = UserConfigV1Alpha1.load_or_create().config.current_client
+    if not config:
+        raise click.BadParameter(
+            "no client specified, and no default client set: specify a client name, or use jmp client use-config ",
+            param_hint="name",
+        )
+
+    exporters = config.list_exporters()
+
+    if output == OutputMode.JSON:
+        click.echo(exporters.dump_json())
+    elif output == OutputMode.YAML:
+        click.echo(exporters.dump_yaml())
+    elif output == OutputMode.NAME:
+        for exporter in exporters.exporters:
+            click.echo(exporter.name)
+    else:
+        columns = ["NAME", "LABELS"]
+
+        def make_row(exporter):
+            return {
+                "NAME": exporter.name,
+                "LABELS": ",".join(("{}={}".format(i[0], i[1]) for i in exporter.labels.items())),
+            }
+
+        rows = list(map(make_row, exporters.exporters))
+        click.echo(make_table(columns, rows))
--- python/packages/jumpstarter/jumpstarter/client/grpc.py
+++ python/packages/jumpstarter/jumpstarter/client/grpc.py
@@ -2,8 +2,10 @@ from __future__ import annotations
 
 from dataclasses import dataclass, field
 
+import yaml
 from grpc.aio import Channel
 from jumpstarter_protocol import client_pb2, client_pb2_grpc
+from pydantic import BaseModel
 
 
 def parse_exporter_identifier(identifier: str) -> (str, str):
@@ -12,13 +14,12 @@ def parse_exporter_identifier(identifier: str) -> (str, str):
         raise ValueError("incorrect number of segments in identifier, expecting 4, got {}".format(len(segments)))
     if segments[0] != "namespaces":
         raise ValueError("incorrect first segment in identifier, expecting namespaces, got {}".format(segments[0]))
-    if segments[2] != "namespaces":
-        raise ValueError("incorrect third segment in identifier, expecting exporters, got {}".format(segments[3]))
+    if segments[2] != "exporters":
+        raise ValueError("incorrect third segment in identifier, expecting exporters, got {}".format(segments[2]))
     return segments[1], segments[3]
 
 
-@dataclass(kw_only=True, slots=True)
-class Exporter:
+class Exporter(BaseModel):
     namespace: str
     name: str
     labels: dict[str, str]
@@ -26,20 +27,25 @@ class Exporter:
     @classmethod
     def from_protobuf(cls, data: client_pb2.Exporter) -> Exporter:
         namespace, name = parse_exporter_identifier(data.name)
-        cls(namespace=namespace, name=name, labels=data.labels)
+        return cls(namespace=namespace, name=name, labels=data.labels)
 
 
-@dataclass(kw_only=True, slots=True)
-class ExporterList:
+class ExporterList(BaseModel):
     exporters: list[Exporter]
     next_page_token: str | None
 
     @classmethod
     def from_protobuf(cls, data: client_pb2.ListExportersResponse) -> ExporterList:
-        cls(
+        return cls(
             exporters=list(map(Exporter.from_protobuf, data.exporters)),
             next_page_token=data.next_page_token,
         )
+
+    def dump_json(self):
+        return self.model_dump_json(indent=4, by_alias=True)
+
+    def dump_yaml(self):
+        return yaml.safe_dump(self.model_dump(mode="json", by_alias=True), indent=2)
 
 
 @dataclass(kw_only=True, slots=True)
