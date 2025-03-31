967c0a22c65b34767fdee18ed929bd9059c1de9a
Nick Cao <nickcao@nichi.co>
2025-03-31 10:39:50 -0400

    fixup! Use xdg config home for client config

--- python/packages/jumpstarter/jumpstarter/config/common.py
+++ python/packages/jumpstarter/jumpstarter/config/common.py
@@ -1,7 +1,5 @@
 from pydantic import BaseModel
-from xdg_base_dirs import (
-    xdg_config_home,
-)
+from xdg_base_dirs import xdg_config_home
 
 CONFIG_API_VERSION = "jumpstarter.dev/v1alpha1"
 CONFIG_PATH = xdg_config_home() / "jumpstarter"
