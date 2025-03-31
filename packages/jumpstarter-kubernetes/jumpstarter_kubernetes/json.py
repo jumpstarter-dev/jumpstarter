04095d916156ffe633fe84c1267d31d8f44f48c6
Nick Cao <nickcao@nichi.co>
2025-03-31 14:03:04 -0400

    Make JsonBaseModel inherit from SerializableBaseModel

--- packages/jumpstarter-kubernetes/jumpstarter_kubernetes/json.py
+++ packages/jumpstarter-kubernetes/jumpstarter_kubernetes/json.py
@@ -1,14 +1,9 @@
-import yaml
-from pydantic import BaseModel, ConfigDict
+from pydantic import ConfigDict
+
+from jumpstarter.common.pydantic import SerializableBaseModel
 
 
-class JsonBaseModel(BaseModel):
+class JsonBaseModel(SerializableBaseModel):
     """A Pydantic BaseModel with additional Jumpstarter JSON options applied."""
-
-    def dump_json(self):
-        return self.model_dump_json(indent=4, by_alias=True)
-
-    def dump_yaml(self):
-        return yaml.safe_dump(self.model_dump(mode="json", by_alias=True), indent=2)
 
     model_config = ConfigDict(arbitrary_types_allowed=True, populate_by_name=True)
