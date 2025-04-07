9113acd07af89bf605897e1ae34423e31c9beadf
Nick Cao <nickcao@nichi.co>
2025-04-07 12:57:54 -0400

    Implement forward-unix command on network client

--- python/packages/jumpstarter-driver-network/jumpstarter_driver_network/client.py
+++ python/packages/jumpstarter-driver-network/jumpstarter_driver_network/client.py
@@ -4,7 +4,7 @@ from threading import Event
 
 import asyncclick as click
 
-from .adapters import DbusAdapter, TcpPortforwardAdapter
+from .adapters import DbusAdapter, TcpPortforwardAdapter, UnixPortforwardAdapter
 from .driver import DbusNetwork
 from jumpstarter.client import DriverClient
 
@@ -20,7 +20,11 @@ class NetworkClient(DriverClient):
         @click.option("--address", default="localhost", show_default=True)
         @click.argument("port", type=int)
         def forward_tcp(address: str, port: int):
-            """Forward local TCP port to remote network connection"""
+            """
+            Forward local TCP port to remote network
+
+            PORT is the TCP port to listen on.
+            """
 
             with TcpPortforwardAdapter(
                 client=self,
@@ -34,6 +38,24 @@ class NetworkClient(DriverClient):
                         click.echo("[{}]:{}".format(host, port))
                     case _:
                         click.echo("{}:{}".format(host, port))
+
+                Event().wait()
+
+        @base.command()
+        @click.argument("path", type=click.Path(), required=False)
+        def forward_unix(path: str | None):
+            """
+            Forward local Unix domain socket to remote network
+
+            PATH is the path of the Unix domain socket to listen on,
+            defaults to a random path under $XDG_RUNTIME_DIR.
+            """
+
+            with UnixPortforwardAdapter(
+                client=self,
+                path=path,
+            ) as addr:
+                click.echo(addr)
 
                 Event().wait()
 
