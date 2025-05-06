61798c747854af381bec323d418a4e7890d493a7
Nick Cao <nickcao@nichi.co>
2025-05-06 15:17:07 -0400

    Fix driver cli test

--- python/packages/jumpstarter-cli-driver/jumpstarter_cli_driver/driver_test.py
+++ python/packages/jumpstarter-cli-driver/jumpstarter_cli_driver/driver_test.py
@@ -1,20 +1,13 @@
-import pytest
 from click.testing import CliRunner
 
 from . import driver
 
 
-@pytest.mark.anyio
-async def test_list_drivers():
+def test_list_drivers():
     runner = CliRunner()
 
-    result = await runner.invoke(
+    result = runner.invoke(
         driver,
         ["list"],
     )
     assert result.exit_code == 0
-
-
-@pytest.fixture
-def anyio_backend():
-    return "asyncio"
