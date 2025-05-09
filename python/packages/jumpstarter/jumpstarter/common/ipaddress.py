3a7c0154594c0f31804414ce4a2ea198258a2b00
Nick Cao <nickcao@nichi.co>
2025-05-09 15:45:58 -0400

    Consolidate impl of get_ip_address
    (cherry picked from commit e812ef01c1aec3f784f256d811ac345427066042)

--- python/packages/jumpstarter/jumpstarter/common/ipaddress.py
+++ python/packages/jumpstarter/jumpstarter/common/ipaddress.py
@@ -1,27 +1,22 @@
 import logging
 import socket
+from ipaddress import ip_address
 
 
-def get_ip_address() -> str:
+def get_ip_address(logger: logging.Logger | None = None) -> str:
     """Get the IP address of the host machine"""
     # Try to get the IP address using the hostname
     hostname = socket.gethostname()
     address = socket.gethostbyname(hostname)
-    # If it returns a bogus address, do it the hard way
-    if not address or address.startswith("127."):
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("1.1.1.1", 0))
-        address = s.getsockname()[0]
+    # If it returns nothing or a loopbadck address, do it the hard way
+    if not address or ip_address(address).is_loopback:
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+                s.connect(("192.175.48.1", 53))  # AS112
+                return s.getsockname()[0]
+        except Exception:
+            if logger:
+                logger.warning("Could not determine default IP address, falling back to 0.0.0.0")
+            return "0.0.0.0"
+
     return address
-
-
-def get_default_ip(logger: logging.Logger | None = None):
-    """Get the IP address of the default route interface"""
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            s.connect(("8.8.8.8", 80))
-            return s.getsockname()[0]
-    except Exception:
-        if logger:
-            logger.warning("Could not determine default IP address, falling back to 0.0.0.0")
-        return "0.0.0.0"
