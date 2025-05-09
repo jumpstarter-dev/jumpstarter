cce6528a47cad59b79ac899682386363a5a0b6ec
Nick Cao <nickcao@nichi.co>
2025-05-09 15:37:29 -0400

    Factor out get_default_ip

--- python/packages/jumpstarter/jumpstarter/common/ipaddress.py
+++ python/packages/jumpstarter/jumpstarter/common/ipaddress.py
@@ -1,3 +1,4 @@
+import logging
 import socket
 
 
@@ -12,3 +13,15 @@ def get_ip_address() -> str:
         s.connect(("1.1.1.1", 0))
         address = s.getsockname()[0]
     return address
+
+
+def get_default_ip(logger: logging.Logger | None = None):
+    """Get the IP address of the default route interface"""
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            s.connect(("8.8.8.8", 80))
+            return s.getsockname()[0]
+    except Exception:
+        if logger:
+            logger.warning("Could not determine default IP address, falling back to 0.0.0.0")
+        return "0.0.0.0"
