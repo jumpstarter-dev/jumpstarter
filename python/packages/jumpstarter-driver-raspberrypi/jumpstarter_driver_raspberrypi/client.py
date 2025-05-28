36312dfaf819c58f6c500f5d779dd366d452b35f
Benny Zlotnik <bzlotnik@redhat.com>
2025-05-28 15:31:08 +0300

    rpi: make rpi a power client
    Signed-off-by: Benny Zlotnik <bzlotnik@redhat.com>

--- python/packages/jumpstarter-driver-raspberrypi/jumpstarter_driver_raspberrypi/client.py
+++ python/packages/jumpstarter-driver-raspberrypi/jumpstarter_driver_raspberrypi/client.py
@@ -1,15 +1,31 @@
 from dataclasses import dataclass
 
+import click
+from jumpstarter_driver_power.client import PowerClient
+
 from jumpstarter.client import DriverClient
 
 
 @dataclass(kw_only=True)
-class DigitalOutputClient(DriverClient):
-    def off(self) -> None:
+class DigitalOutputClient(PowerClient):
+    def on(self):
+        """Turn power on"""
+        self.call("on")
+
+    def off(self):
+        """Turn power off"""
         self.call("off")
 
-    def on(self) -> None:
-        self.call("on")
+    def cli(self):
+        @click.group()
+        def gpio():
+            """GPIO power control commands"""
+            pass
+
+        for cmd in super().cli().commands.values():
+            gpio.add_command(cmd)
+
+        return gpio
 
 
 @dataclass(kw_only=True)
