e74452b4745a39232c21802127b7305e7fac19b1
Nick Cao <nickcao@nichi.co>
2025-06-24 14:11:10 -0400

    Implement progress bar with rich instead of tqdm

--- python/packages/jumpstarter/jumpstarter/streams/progress.py
+++ python/packages/jumpstarter/jumpstarter/streams/progress.py
@@ -1,94 +1,21 @@
 import logging
 from dataclasses import dataclass, field
+from datetime import datetime, timedelta
+from io import StringIO
 
 from anyio.abc import ObjectStream
-from tqdm import tqdm
+from rich.console import Console
+from rich.progress import (
+    BarColumn,
+    FileSizeColumn,
+    Progress,
+    TaskID,
+    TextColumn,
+    TimeElapsedColumn,
+    TransferSpeedColumn,
+)
 
-TQDM_KWARGS = {
-    "unit": "B",
-    "unit_scale": True,
-    "unit_divisor": 1024,
-}
-
-LOGGER = logging.getLogger(__name__)
-
-
-# Copied from https://github.com/tqdm/tqdm/pull/1172
-class logging_tqdm(tqdm):
-    """
-    A version of tqdm that outputs the progress bar
-    to Python logging instead of the console.
-    The progress will be logged with the info level.
-    Parameters
-    ----------
-    logger   : logging.Logger, optional
-      Which logger to output to (default: logger.getLogger('tqdm.contrib.logging')).
-    All other parameters are passed on to regular tqdm,
-    with the following changed default:
-    mininterval: 1
-    bar_format: '{desc}{percentage:3.0f}%{r_bar}'
-    desc: 'progress: '
-    Example
-    -------
-    ```python
-    import logging
-    from time import sleep
-    from tqdm.contrib.logging import logging_tqdm
-    LOG = logging.getLogger(__name__)
-    if __name__ == '__main__':
-        logging.basicConfig(level=logging.INFO)
-        for _ in logging_tqdm(range(10), mininterval=1, logger=LOG):
-            sleep(0.3)  # assume processing one item takes less than mininterval
-    ```
-    """
-
-    def __init__(
-        self,
-        *args,
-        # logger=None,  # type: logging.Logger
-        # mininterval=1,  # type: float
-        # bar_format='{desc}{percentage:3.0f}%{r_bar}',  # type: str
-        # desc='progress: ',  # type: str
-        **kwargs,
-    ):
-        if len(args) >= 2:
-            # Note: Due to Python 2 compatibility, we can't declare additional
-            #   keyword arguments in the signature.
-            #   As a result, we could get (due to the defaults below):
-            #     TypeError: __init__() got multiple values for argument 'desc'
-            #   This will raise a more descriptive error message.
-            #   Calling dummy init to avoid attribute errors when __del__ is called
-            super(logging_tqdm, self).__init__([], disable=True)
-            raise ValueError("only iterable may be used as a positional argument")
-        tqdm_kwargs = kwargs.copy()
-        self._logger = tqdm_kwargs.pop("logger", None)
-        tqdm_kwargs.setdefault("mininterval", 1)
-        tqdm_kwargs.setdefault("bar_format", "{desc}{percentage:3.0f}%{r_bar}")
-        tqdm_kwargs.setdefault("desc", "progress: ")
-        self._last_log_n = -1
-        super(logging_tqdm, self).__init__(*args, **tqdm_kwargs)
-
-    def _get_logger(self):
-        if self._logger is not None:
-            return self._logger
-        return LOGGER
-
-    def display(self, msg=None, pos=None):
-        if not self.n:
-            # skip progress bar before having processed anything
-            LOGGER.debug("ignoring message before any progress: %r", self.n)
-            return
-        if self.n == self._last_log_n:
-            # avoid logging for the same progress multiple times
-            LOGGER.debug("ignoring log message with same n: %r", self.n)
-            return
-        self._last_log_n = self.n
-        if msg is None:
-            msg = self.__str__()
-        if not msg:
-            LOGGER.debug("ignoring empty message: %r", msg)
-            return
-        self._get_logger().info("%s", msg)
+logger = logging.getLogger(__name__)
 
 
 @dataclass(kw_only=True)
@@ -96,36 +23,54 @@ class ProgressStream(ObjectStream[bytes]):
     stream: ObjectStream
     logging: bool = False
 
-    __recv: tqdm = field(init=False, default=None)
-    __send: tqdm = field(init=False, default=None)
+    __prog: Progress | None = field(init=False, default=None)
+    __recv: TaskID | None = field(init=False, default=None)
+    __send: TaskID | None = field(init=False, default=None)
+    __last: datetime = field(init=False, default_factory=datetime.now)
+
+    def __post_init__(self):
+        if hasattr(super(), "__post_init__"):
+            super().__post_init__()
+
+        self.__prog = Progress(
+            TextColumn("[progress.description]{task.description}"),
+            BarColumn(),
+            TransferSpeedColumn(),
+            FileSizeColumn(),
+            TimeElapsedColumn(),
+            disable=self.logging,
+        )
 
     def __del__(self):
-        if self.__recv is not None:
-            self.__recv.close()
-        if self.__send is not None:
-            self.__send.close()
+        self.__prog.stop()
 
     async def receive(self):
+        if self.__recv is None:
+            self.__prog.start()
+            self.__recv = self.__prog.add_task("transfer", total=None)
+
         item = await self.stream.receive()
 
-        if self.__recv is None:
-            if self.logging:
-                self.__recv = logging_tqdm(desc="transfer", **TQDM_KWARGS)
-            else:
-                self.__recv = tqdm(desc="transfer", **TQDM_KWARGS)
-
-        self.__recv.update(len(item))
+        self.__prog.advance(self.__recv, len(item))
+        if self.logging and (datetime.now() - self.__last > timedelta(seconds=2)):
+            self.__last = datetime.now()
+            console = Console(file=StringIO())
+            console.print(self.__prog.get_renderable())
+            logger.info(console.file.getvalue().rstrip())
 
         return item
 
     async def send(self, item):
         if self.__send is None:
-            if self.logging:
-                self.__send = logging_tqdm(desc="transfer", **TQDM_KWARGS)
-            else:
-                self.__send = tqdm(desc="transfer", **TQDM_KWARGS)
+            self.__prog.start()
+            self.__send = self.__prog.add_task("transfer", total=None)
 
-        self.__send.update(len(item))
+        self.__prog.advance(self.__recv, len(item))
+        if self.logging and (datetime.now() - self.__last > timedelta(seconds=2)):
+            self.__last = datetime.now()
+            console = Console(file=StringIO())
+            console.print(self.__prog.get_renderable())
+            logger.info(console.file.getvalue().rstrip())
 
         await self.stream.send(item)
 
