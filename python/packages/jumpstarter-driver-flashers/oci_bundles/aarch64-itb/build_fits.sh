a8120feeb3253980d1ef94c27fd1ef8f8dadff01
Michal Skrivanek <michal.skrivanek@redhat.com>
2025-07-03 09:59:45 +0200

    run the build only in a container
    or spawn one if invoked directly

--- python/packages/jumpstarter-driver-flashers/oci_bundles/aarch64-itb/build_fits.sh
+++ python/packages/jumpstarter-driver-flashers/oci_bundles/aarch64-itb/build_fits.sh
@@ -1,37 +1,31 @@
 #!/usr/bin/env bash
 
-# If this script may be sourced, you want ${BASH_SOURCE[0]} instead of $0
-SOURCE="${BASH_SOURCE[0]}"
-while [ -L "$SOURCE" ]; do
-  SOURCE="$(readlink "$SOURCE")"
-done
+cd "$(dirname "$0")"
 
-SCRIPT_DIR="$(cd "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
-cd "${SCRIPT_DIR}"
+# run only in a container
+if [[ -z "$container" && ! -f /.dockerenv ]]; then
+    exec podman run --rm -it -v $(pwd):/host -w /host fedora:42 "$0" "$@"
+else
+	set -euo pipefail
+	BUILDROOT_DIR="/var/tmp/buildroot"
 
-BUILDROOT_DIR="${SCRIPT_DIR}/buildroot"
+	dnf install --setopt=install_weak_deps=false -y git make gcc gcc-c++ which file diffutils \
+		wget cpio rsync bc lzop zip patch perl tar qemu-system-aarch64 qemu-img unzboot \
+		uboot-tools kmod awk zstd
 
-set -euo pipefail
-
-
-sudo dnf install --setopt=install_weak_deps=false -y git make gcc gcc-c++ which file diffutils \
-	wget cpio rsync bc lzop zip patch perl tar qemu-system-aarch64 qemu-img unzboot \
-	uboot-tools kmod awk zstd
-
-if [[ ! -d "${BUILDROOT_DIR}" ]]; then
 	git clone --depth 1 --branch 2025.05  https://github.com/buildroot/buildroot "${BUILDROOT_DIR}"
+
+	# build default buildroot kernel & initramfs
+	cp buildroot_defconfig  "${BUILDROOT_DIR}/configs/"
+	cp -R overlay "${BUILDROOT_DIR}"
+	( cd "${BUILDROOT_DIR}"; make buildroot_defconfig && make )
+	mkimage -f buildroot.its data/flasher-buildroot.itb
+	rm -rf "${BUILDROOT_DIR}/overlay"
+
+	# replace kernel with kernel-automotive and rebuild
+	cp -R overlay "${BUILDROOT_DIR}"
+	./replace_kernel.sh "${BUILDROOT_DIR}/overlay"
+	( cd "${BUILDROOT_DIR}" && make )
+	mkimage -f automotive.its data/flasher-automotive.itb
+	rm -rf "${BUILDROOT_DIR}/overlay"
 fi
-
-# build default buildroot kernel & initramfs
-cp buildroot_defconfig  "${BUILDROOT_DIR}/configs/"
-cp -R overlay "${BUILDROOT_DIR}"
-( cd "${BUILDROOT_DIR}"; make buildroot_defconfig && make )
-mkimage -f buildroot.its data/flasher-buildroot.itb
-rm -rf "${BUILDROOT_DIR}/overlay"
-
-# replace kernel with kernel-automotive and rebuild
-cp -R overlay "${BUILDROOT_DIR}"
-./replace_kernel.sh "${BUILDROOT_DIR}/overlay"
-( cd "${BUILDROOT_DIR}" && make )
-mkimage -f automotive.its data/flasher-automotive.itb
-rm -rf "${BUILDROOT_DIR}/overlay"
