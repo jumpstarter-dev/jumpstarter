5e15eb1c21104aa85a5f50bc634cba6df87bac3f
Michal Skrivanek <michal.skrivanek@redhat.com>
2025-09-22 15:02:17 +0200

    bump to buildroot 2025.08
    for more recent kernel 6.16.5 needed for NXP S32G3
    
    (cherry picked from commit da1e904d04b04c6d82682475d6da4b1b44e72c91)

--- packages/jumpstarter-driver-flashers/oci_bundles/aarch64-itb/build_fits.sh
+++ packages/jumpstarter-driver-flashers/oci_bundles/aarch64-itb/build_fits.sh
@@ -13,7 +13,7 @@ else
 		wget cpio rsync bc lzop zip patch perl tar qemu-system-aarch64 qemu-img unzboot \
 		uboot-tools kmod awk zstd
 
-	git clone --depth 1 --branch 2025.05  https://github.com/buildroot/buildroot "${BUILDROOT_DIR}"
+	git clone --depth 1 --branch 2025.08  https://github.com/buildroot/buildroot "${BUILDROOT_DIR}"
 
 	# build default buildroot kernel & initramfs
 	cp buildroot_defconfig  "${BUILDROOT_DIR}/configs/"
