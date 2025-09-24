d4f5e3b4984661a77206a8aa9fe3548b0a7bc9c9
Michal Skrivanek <michal.skrivanek@redhat.com>
2025-09-24 12:29:22 +0200

    extract subimages manually on TI
    we do not have larger BOOTM_LEN set for TI uboots, so let's extract the bits manually and use booti to boot
    
    (cherry picked from commit 3ceb233d9241f3e1468eb3f21ed9d55f0143735c)

--- python/packages/jumpstarter-driver-flashers/oci_bundles/aarch64-itb/kernel_fedora.sh
+++ python/packages/jumpstarter-driver-flashers/oci_bundles/aarch64-itb/kernel_fedora.sh
@@ -7,6 +7,44 @@ KMOD=(
 	# S32G3 networking
 	dwmac_s32
 	micrel
+	# TI storage
+	sdhci_am654
+	mmc_block
+	# TI networking
+	phy_gmii_sel
+	syscon_clk
+	dp83867
+	reset_ti_sci
+	davinci_mdio
+	am65_cpts
+	gpio_davinci
+	k3_udma
+	rti_wdt
+	i2c_omap
+	i2c_mux_pca954x
+	irq_ti_sci_intr
+	omap_mailbox
+	omap_hwspinlock
+	phy_j721e_wiz
+	ti_k3_r5_remoteproc
+	ti_k3_dsp_remoteproc
+	k3_j72xx_bandgap
+	pinctrl_tps6594
+	tps6594_pfsm
+	tps6594_i2c
+	tps6594_regulator
+	rtc_tps6594
+	phy_cadence_torrent
+	mux_core
+	mux_gpio
+	mux_mmio
+	virtio_rpmsg_bus
+	rpmsg_ctrl
+	irq_ti_sci_inta
+	optee
+	omap_rng
+	optee_rng
+	ti_am65_cpsw_nuss
 )
 
 [ $# -ne 1 ] && { echo "$0 [target_overlay_dir]"; exit 1; }
@@ -20,7 +58,7 @@ if [ "$(uname -m)" != "aarch64" ]; then
   exit 1
 fi
 unzboot /usr/lib/modules/$KVER/vmlinuz vmlinuz
-lz4 -12 vmlinuz vmlinuz.lz4
+lz4 -f -12 vmlinuz vmlinuz.lz4
 ln -sfn /lib/modules/$KVER/dtb dtb
 echo "building required modules list ..."
 for mod in ${KMOD[@]}; do
@@ -37,14 +75,18 @@ sed -nr 's|^insmod ||p' < ./kernel-fedora/modlist | while read mod; do
 	xz -dc "$mod.xz" > "$ODIR$mod"
 done
 
-echo "adding modules start-up script to overlay ..."
-script=$ODIR/etc/init.d/S01modules
-cat >$script <<EOF
-#!/bin/sh
+echo "updating module deps ..."
+depmod --errsyms --filesyms /lib/modules/$KVER/System.map --basedir $1 $KVER
 
-if [ "\$1" = "start" ]; then
-$(cat ./kernel-fedora/modlist)
-fi
-EOF
-chmod +x $script
+# relying on udev now
+#echo "adding modules start-up script to overlay ..."
+#script=$ODIR/etc/init.d/S01modules
+#cat >$script <<EOF
+##!/bin/sh
+#
+#if [ "\$1" = "start" ]; then
+#$(cat ./kernel-fedora/modlist)
+#fi
+#EOF
+#chmod +x $script
 
