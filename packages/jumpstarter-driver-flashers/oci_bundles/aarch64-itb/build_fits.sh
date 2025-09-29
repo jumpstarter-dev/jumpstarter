7b875846085c4487354d78cbb78c2a4826a074ec
Michal Skrivanek <michal.skrivanek@redhat.com>
2025-09-29 15:10:25 +0200

    unzboot with zstd landed in Fedora 42
    dropping the workaround
    
    (cherry picked from commit 60f51720340b09f67301ffc1bca97b99333e889f)

--- packages/jumpstarter-driver-flashers/oci_bundles/aarch64-itb/build_fits.sh
+++ packages/jumpstarter-driver-flashers/oci_bundles/aarch64-itb/build_fits.sh
@@ -13,10 +13,6 @@ else
 		wget cpio rsync bc lzop zip patch perl tar qemu-system-aarch64 qemu-img unzboot \
 		uboot-tools kmod awk zstd lz4  kernel dtc rpm-build
 
-	# FIXME remove in Fedora 43
-	# until unzboot is updated, use the build directly
-	rpm -Uvh https://kojipkgs.fedoraproject.org//packages/unzboot/0.1~git.20250502.0c0c3ad/2.fc43/aarch64/unzboot-0.1~git.20250502.0c0c3ad-2.fc43.aarch64.rpm
-
 	git clone --depth 1 --branch 2025.08  https://github.com/buildroot/buildroot "${BUILDROOT_DIR}"
 
 	# build buildroot (initramfs)
