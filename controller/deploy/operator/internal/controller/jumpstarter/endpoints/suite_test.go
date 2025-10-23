0eb3e07655e73f9e8d64f0784cdcdcc449b6ab84
Miguel Angel Ajo Pelayo <miguelangel@ajo.es>
2025-10-23 12:37:07 +0000

    operator: remove envtest duplication

--- controller/deploy/operator/internal/controller/jumpstarter/endpoints/suite_test.go
+++ controller/deploy/operator/internal/controller/jumpstarter/endpoints/suite_test.go
@@ -18,7 +18,6 @@ package endpoints
 
 import (
 	"context"
-	"os"
 	"path/filepath"
 	"testing"
 
@@ -33,6 +32,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	operatorv1alpha1 "github.com/jumpstarter-dev/jumpstarter-controller/deploy/operator/api/v1alpha1"
+	"github.com/jumpstarter-dev/jumpstarter-controller/deploy/operator/internal/controller/testutils"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -71,8 +71,8 @@ var _ = BeforeSuite(func() {
 	}
 
 	// Retrieve the first found binary directory to allow running tests from IDEs
-	if getFirstFoundEnvTestBinaryDir() != "" {
-		testEnv.BinaryAssetsDirectory = getFirstFoundEnvTestBinaryDir()
+	if binaryDir := testutils.GetFirstFoundEnvTestBinaryDir(6); binaryDir != "" {
+		testEnv.BinaryAssetsDirectory = binaryDir
 	}
 
 	// cfg is defined in this file globally.
@@ -91,26 +91,3 @@ var _ = AfterSuite(func() {
 	err := testEnv.Stop()
 	Expect(err).NotTo(HaveOccurred())
 })
-
-// getFirstFoundEnvTestBinaryDir locates the first binary in the specified path.
-// ENVTEST-based tests depend on specific binaries, usually located in paths set by
-// controller-runtime. When running tests directly (e.g., via an IDE) without using
-// Makefile targets, the 'BinaryAssetsDirectory' must be explicitly configured.
-//
-// This function streamlines the process by finding the required binaries, similar to
-// setting the 'KUBEBUILDER_ASSETS' environment variable. To ensure the binaries are
-// properly set up, run 'make setup-envtest' beforehand.
-func getFirstFoundEnvTestBinaryDir() string {
-	basePath := filepath.Join("..", "..", "..", "..", "..", "..", "bin", "k8s")
-	entries, err := os.ReadDir(basePath)
-	if err != nil {
-		logf.Log.Error(err, "Failed to read directory", "path", basePath)
-		return ""
-	}
-	for _, entry := range entries {
-		if entry.IsDir() {
-			return filepath.Join(basePath, entry.Name())
-		}
-	}
-	return ""
-}
