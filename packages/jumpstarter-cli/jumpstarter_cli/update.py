b23b28736d3953203c8f6018a5138b767515f1fb
Benny Zlotnik <bzlotnik@redhat.com>
2025-12-02 16:31:21 +0200

    cli: support lease transfer
    depends on https://github.com/jumpstarter-dev/jumpstarter-controller/pull/199
    
    Signed-off-by: Benny Zlotnik <bzlotnik@redhat.com>

--- packages/jumpstarter-cli/jumpstarter_cli/update.py
+++ packages/jumpstarter-cli/jumpstarter_cli/update.py
@@ -22,20 +22,32 @@ def update():
 @click.argument("name")
 @opt_duration_partial(required=False)
 @opt_begin_time
+@click.option(
+    "--client",
+    type=str,
+    default=None,
+    help="Transfer lease to a different client (format: namespaces/{namespace}/clients/{client})",
+)
 @opt_output_all
 @handle_exceptions_with_reauthentication(relogin_client)
-def update_lease(config, name: str, duration: timedelta | None, begin_time: datetime | None, output: OutputType):
+def update_lease(
+    config, name: str, duration: timedelta | None, begin_time: datetime | None, client: str | None, output: OutputType
+):
     """
     Update a lease
 
-    Update the duration and/or begin time of an existing lease.
-    At least one of --duration or --begin-time must be specified.
+    Update the duration, begin time, and/or owner of an existing lease.
+    At least one of --duration, --begin-time, or --client must be specified.
+
+    To transfer a lease to another client, use the --client option with the
+    full client identifier (e.g., namespaces/default/clients/other-client).
+
     Updating the begin time of an already active lease is not allowed.
     """
 
-    if duration is None and begin_time is None:
-        raise click.UsageError("At least one of --duration or --begin-time must be specified")
+    if duration is None and begin_time is None and client is None:
+        raise click.UsageError("At least one of --duration, --begin-time, or --client must be specified")
 
-    lease = config.update_lease(name, duration=duration, begin_time=begin_time)
+    lease = config.update_lease(name, duration=duration, begin_time=begin_time, client=client)
 
     model_print(lease, output)
