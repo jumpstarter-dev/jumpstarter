f03bf512aa12fbf85d4907dd588a930c33347675
Albert Esteve <aesteve@redhat.com>
2025-12-12 09:38:02 +0100

    novnc: Improve docstrings for coverage
    Signed-off-by: Albert Esteve <aesteve@redhat.com>

--- python/packages/jumpstarter-driver-network/jumpstarter_driver_network/adapters/novnc.py
+++ python/packages/jumpstarter-driver-network/jumpstarter_driver_network/adapters/novnc.py
@@ -11,6 +11,21 @@ from jumpstarter.streams.common import forward_stream
 @blocking
 @asynccontextmanager
 async def NovncAdapter(*, client: DriverClient, method: str = "connect", encrypt: bool = True):
+    """
+    Provide a noVNC URL that proxies a temporary local TCP listener to a remote
+    driver stream via a WebSocket bridge.
+
+    Parameters:
+        client (DriverClient): Client used to open the remote stream that will be
+                               bridged to the local listener.
+        method (str): Name of the async stream method to call on the client (default "connect").
+        encrypt (bool): If True request an encrypted (TLS) vnc connection;
+                        if False request an unencrypted vnc connection.
+
+    Returns:
+        str: The URL to connect to the VNC session.
+    """
+
     async def handler(conn):
         async with conn:
             async with client.stream_async(method) as stream:
