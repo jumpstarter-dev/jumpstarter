27c04664b54a8d516cce2155c14fafafd3834483
Nick Cao <nickcao@nichi.co>
2026-01-27 10:14:22 -0500

    treewide: use fedora 43

--- python/packages/jumpstarter-driver-qemu/jumpstarter_driver_qemu/driver_test.py
+++ python/packages/jumpstarter-driver-qemu/jumpstarter_driver_qemu/driver_test.py
@@ -71,13 +71,13 @@ def test_driver_qemu(tmp_path, ovmf):
         username = qemu.username
         password = qemu.password
 
-        cached_image = Path(__file__).parent.parent / "images" / f"Fedora-Cloud-Base-Generic-41-1.4.{arch}.qcow2"
+        cached_image = Path(__file__).parent.parent / "images" / f"Fedora-Cloud-Base-Generic-43-1.6.{arch}.qcow2"
 
         if cached_image.exists():
             qemu.flasher.flash(cached_image.resolve())
         else:
             qemu.flasher.flash(
-                f"pub/fedora/linux/releases/41/Cloud/{arch}/images/Fedora-Cloud-Base-Generic-41-1.4.{arch}.qcow2",
+                f"pub/fedora/linux/releases/43/Cloud/{arch}/images/Fedora-Cloud-Base-Generic-43-1.6.{arch}.qcow2",
                 operator=Operator("http", endpoint="https://download.fedoraproject.org"),
             )
 
@@ -97,7 +97,7 @@ def test_driver_qemu(tmp_path, ovmf):
             p.expect_exact(f"[{username}@{hostname} ~]$")
 
         with qemu.shell() as s:
-            assert s.run("uname -r").stdout.strip() == f"6.11.4-301.fc41.{arch}"
+            assert s.run("uname -r").stdout.strip() == f"6.17.1-300.fc43.{arch}"
 
         qemu.power.off()
 
